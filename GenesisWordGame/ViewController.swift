766a34de4230d525131a28c0931ca7f28e1d1c3b
Rikuto0623 <rikuriku0623to@gmail.com>
2026-06-02 19:32:34 +0900

    Add files via upload

---
+++ GenesisWordGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GenesisWordGame
+//
+//  Created by 鈴木久美 on 2026/06/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
