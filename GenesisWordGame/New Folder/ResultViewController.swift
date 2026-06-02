54f01ae56796e31be97addd9fa00b90345ebd35f
Rikuto <93rainbow@gmail.com>
2026-06-03 08:48:20 +0900

    Commit God

--- GenesisWordGame/New Folder/ResultViewController.swift
+++ GenesisWordGame/New Folder/ResultViewController.swift
@@ -23,6 +23,10 @@ class ResultViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        // 保存されたポイント取得
+        point = UserDefaults.standard.integer(forKey: "POINT")
+        
         // ポイント表示
         pointLabel.text = "現在のポイント: \(point)"
 
